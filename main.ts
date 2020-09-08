26e889807d65c380fc66904c39dd5b2e5676dd72
tomi34 <66636695+tomi34@users.noreply.github.com>
2020-09-08 17:15:34 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+function tempokiirasa () {
+    basic.showNumber(tempo)
+    basic.pause(100)
+    basic.clearScreen()
+}
+input.onButtonPressed(Button.A, function () {
+    tempo += 1
+    tempokiirasa()
+})
+input.onButtonPressed(Button.B, function () {
+    tempo += -1
+    tempokiirasa()
+})
+let tempo = 0
+tempo = 1
+tempokiirasa()
+basic.forever(function () {
+    basic.pause(1000 / tempo)
+    music.playTone(262, music.beat(BeatFraction.Sixteenth))
+})
